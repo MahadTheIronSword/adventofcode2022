22da287d0fa4866245ffa568b615d48763e9acf2
MahadTheIronSword <mahadsyed125@gmail.com>
2022-12-06 19:04:58 -0600

    Day 6 part 2

--- Source/Days/Day6/Day6.cs
+++ Source/Days/Day6/Day6.cs
@@ -1,6 +1,9 @@
 namespace AdventOfCode;
 
 public static class Day6 {
+    // 4 for part 1 14 for part 2
+    public static readonly int StartMarker = 14;
+
     public static void Run() {
         string input = System.IO.File.ReadAllText("Source/Days/Day6/Input.txt");
 
@@ -13,8 +16,8 @@ public static class Day6 {
 
             foundCharacters.Add(c);
 
-            if (foundCharacters.Count >= 4) {
-                List<char> lastFour = foundCharacters.GetRange(foundCharacters.Count - 4, 4);
+            if (foundCharacters.Count >= StartMarker) {
+                List<char> lastFour = foundCharacters.GetRange(foundCharacters.Count - StartMarker, StartMarker);
 
                 if (lastFour.Count == lastFour.Distinct().Count()) {
                     break;
